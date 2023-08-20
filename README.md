d1b6ac97b57266c806e5dbe0ea7e3f16c7aedf5b
Juan Landi <58494540+jdlandi@users.noreply.github.com>
2023-08-20 18:04:39 -0500

    Remodel Files
    Restructuring and improving the readability and layout of code
    Removing dead code
    Adding docstrings

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# citadel_final_project
+# Citadel Finance Project
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [In-Depth Feature Breakdown](#in-depth-feature-breakdown)
+- [Technical Architecture](#technical-architecture)
+- [Project Structure](#project-structure)
+- [Development Process](#development-process)
+- [Code Explanation](#code-explanation)
+- [Usage](#usage)
+- [Technologies Used](#technologies-used)
+- [Contributing](#contributing)
+- [Author](#author)
+- [License](#license)
+
+## Overview
+
+Citadel Finance is a cutting-edge web application designed to provide users with a comprehensive suite of financial tools and services. Whether you're a seasoned investor or just starting your financial journey, Citadel Finance offers a range of features to help you make informed decisions and stay ahead of the market.
+
+## Features
+
+1. **User Authentication**: Secure sign-up and login functionality with input verification and password hashing.
+2. **Flashcards**: Generate AI-powered flashcards for learning and information retention.
+3. **Portfolio Management**: Manage and track stock portfolios with insights into holdings and performance.
+4. **News**: Search for news articles based on specific queries and get AI-powered summaries.
+5. **Forex Trading**: Get insights into forex trading with exchange rates and trading pairs.
+6. **AI Assistance**: Leverage AI models for flashcards, news summarization, and more.
+
+## In-Depth Feature Breakdown
+
+- **AI-Powered Flashcards**: Citadel Finance uses OpenAI models to generate insightful flashcards based on user queries. This feature allows users to learn about financial concepts, stock analysis, forex trading, and more in an interactive and engaging way.
+- **Portfolio Management**: Users can create and manage multiple stock portfolios, track their holdings, and view performance metrics. The application provides real-time data on stock prices, price differences, daily highs and lows, and more.
+- **News Summarization**: Users can search for news articles on specific topics and get AI-powered summaries. This feature helps users stay updated on market trends, company news, and financial developments without spending hours reading articles.
+- **Forex Trading Insights**: Citadel Finance offers insights into forex trading by providing exchange rates, profitable trading pairs, and potential profit calculations. Users can explore different currency pairs and make informed trading decisions.
+
+## Technical Architecture
+
+Citadel Finance is built on the Flask web application framework for Python. The backend is powered by Python, and the frontend is developed using HTML, CSS, and JavaScript. The application uses MongoDB for data storage and OpenAI models for AI-powered assistance. Various APIs are used to retrieve news articles and forex trading data.
+
+## Project Structure
+
+- **`app.py`**: Main Flask application, handles routing, user authentication, and web page rendering.
+- **`datatypes.py`**: Defines custom data types and utility functions for stocks, forex, flashcards, and more.
+- **`model.py`**: Contains functions related to AI models and APIs for news and financial data.
+- **`/templates`**: Contains HTML templates for rendering web pages.
+- **`/static`**: Contains static assets such as CSS stylesheets and images.
+
+## Development Process
+
+The development of Citadel Finance followed an iterative and agile approach. The project began with a thorough planning phase, during which the features, technical architecture, and design were finalized. The development process was divided into several sprints, with each sprint focused on a specific set of features or improvements.
+
+During each sprint, the code was developed, tested, and integrated into the main codebase. Continuous integration and continuous deployment (CI/CD) practices were followed to ensure that the application remained stable and functional throughout the development process.
+
+The application underwent rigorous testing, including unit testing, integration testing, and user acceptance testing (UAT). Feedback from UAT was used to further refine and optimize the application.
+
+## Code Explanation
+
+- **User Authentication**: Citadel Finance uses a secure authentication system that includes input verification, password hashing, and cookie management. User passwords are hashed using the SHA-256 algorithm for added security. The `input_verification` function ensures that usernames and passwords meet specific criteria, such as minimum length.
+- **Flashcards Generation**: The `flashcards` and `save_flashcards` routes in `app.py` handle flashcard generation and saving. The `ask_ai` function in `model.py` uses OpenAI models to generate flashcards based on user queries.
+- **Portfolio Management**: The `portfolio` route in `app.py` allows users to manage their stock portfolios. The `gen_stock` function in `datatypes.py` generates stock data, including ticker, original price, current price, price difference, and daily highs and lows.
+- **News Summarization**: The `news` route in `app.py` handles news article searches and summarization. The `get_search` function in `model.py` retrieves news articles based on user queries and provides summaries using AI models.
+- **Forex Trading Insights**: The `currency` route in `app.py` provides forex trading insights. The `calculate_forex` function in `datatypes.py` calculates potential profits for different currency pairs based on exchange rates.
+
+## Usage
+
+This repository showcases the code powering the live version of Citadel Finance. It is not intended for local deployment. Visit the live version at [www.startcitadel.com](https://www.startcitadel.com).
+
+## Technologies Used
+
+- **Flask**: Web application framework for Python.
+- **OpenAI**: AI models for flashcards and assistance.
+- **NewsAPI**: API for news articles.
+- **ExchangeRate.host**: API for forex data.
+- **MongoDB**: Database for data storage.
+- **HTML/CSS/JavaScript**: Frontend development.
+
+## Contributing
+
+While this project is primarily a showcase, contributions are welcome. Submit a pull request or open an issue for suggestions or improvements.
+
+## Authors
+
+[Jay Landi](https://www.linkedin.com/in/jdlandi/) | California Institute of Technology
+
+[Lucia Wang](https://www.linkedin.com/in/lucia-wang-a3a58b248/) | Massachusetts Institute of Technology
+
+
+## License
+
+This project is licensed under the MIT License.
--- app.py
+++ app.py
@@ -1,196 +1,247 @@
-# -- Import section --
-from flask import Flask
-from flask import render_template, request, redirect, make_response
-from pymongo import MongoClient
+# Standard libraries
+import os
 
+# Flask-related libraries
+from flask import Flask, render_template, request, redirect, make_response
+from flask_pymongo import PyMongo
+
+# Local modules
 import model
 import datatypes
 
-
-# -- INITIALIZE APP --
+# Initialize Flask app
 app = Flask(__name__)
 
-# CONFIGURE MONGO DB
-# create the clent
-username = "final"
-password = "1BB8jrqKVpLOOYFH"
-url = f"mongodb+srv://{username}:{password}@cluster0.wohlkjh.mongodb.net/"
-client = MongoClient(url)
+# Check for required environment variables
+required_env_vars = ['NEWS_API_KEY', 'OPENAI_API_KEY', 'OPENAI_ORG', 'MONGO_PWD', 'MONGO_USR']
+for var in required_env_vars:
+    if os.environ.get(var) is None:
+        raise Exception(f'Environment variable {var} not found')
 
-# our database manager object
-db = client["final"]
+# Configure MongoDB
+mongo_username = os.environ.get("MONGO_USR")
+mongo_password = os.environ.get("MONGO_PWD")
+mongo_uri = f"mongodb+srv://{mongo_username}:{mongo_password}@cluster0.wohlkjh.mongodb.net/final"
+app.config["MONGO_URI"] = mongo_uri
+db = PyMongo(app).db
 
-# HOME PAGE
+
 @app.route("/home")
-def dashboard():
-  # collection = db.profiles
-  # events = collection.find({})
-  username = request.cookies.get("username")
-  return render_template("index.html", user=username)
+def home():
+    """
+    Render the home page with the username retrieved from cookies.
 
-#portfolio
+    Returns:
+        str: Rendered HTML of the home page.
+    """
+    username = request.cookies.get("username")
+    if not username:
+        return render_template("index.html")
+    return render_template("index.html", user=username)
+
+
 @app.route("/portfolio", methods=["POST", "GET"])
-def stock_checker():
-  collection = db.profiles
-  user_portfolios = collection.find_one({"username": request.cookies.get("username")})["portfolios"]
-  username = request.cookies.get("username")
-  if request.method == "POST":
-    input = request.form["new_stock"]
-    ticker = model.ask_ai(input, "stock_input") 
-    stock_list = [datatypes.gen_stock(ticker)]
-    # updated_list = datatypes.updated_stock_prices(stock_list)
+def portfolio():
+    """
+    Render the portfolio page. Add a new stock to the user's portfolio if the request method is POST.
+
+    Returns:
+        str: Rendered HTML of the portfolio page.
+    """
+    collection = db.profiles
+    username = request.cookies.get("username")
+    user_data = collection.find_one({"username": username})
+    if not user_data:
+        return render_template("portfolio.html", portfolios=[])
+
+    user_portfolios = user_data["portfolios"]
+
+    if request.method == "POST":
+        add_stock_to_portfolio(username, user_portfolios, collection)
+
+    return render_template("portfolio.html", portfolios=user_portfolios)
+
+
+def add_stock_to_portfolio(username, user_portfolios, collection):
+    """
+    Add a new stock to the user's portfolio.
+
+    Args:
+        username (str): The username of the user.
+        user_portfolios (list): The user's portfolio data.
+    """
+    new_stock = request.form["new_stock"]
+    ticker = model.ask_ai(new_stock, "stock_input")
+    stock_data = [datatypes.gen_stock(ticker)]
+    news_data = model.get_search(ticker, 5)
+    summary = model.ask_ai(str(news_data), "summ_news")
+
     new_portfolio = {
-      "name" : ticker,
-      "creation" : "4-9-2994",
-      "private": True,
-      "info": "",
-      "stocks": stock_list
+        "name": ticker,
+        "creation": "4-9-2994",
+        "private": True,
+        "info": summary,
+        "stocks": stock_data
     }
-    gathered_news = model.get_search(ticker, 5)
-    new_portfolio["info"] = model.ask_ai(str(gathered_news), "summ_news")
 
     user_portfolios.append(new_portfolio)
-    collection.update_one({"username": request.cookies.get('username')}, {"$set" : {"portfolios" : user_portfolios}})
-
-    return render_template("portfolio.html", portfolios = user_portfolios, user=username)
+    collection.update_one({"username": username}, {"$set": {"portfolios": user_portfolios}})
 
 
-  else:
-    return render_template("portfolio.html", portfolios=user_portfolios, user=username)
-
-#news
 @app.route("/news", methods=["POST", "GET"])
 def news():
-  username = request.cookies.get("username")
-  if request.method == "POST":
-    query = request.form["news_query"]
-    num = request.form["num_articles"]
-    out = model.get_search(query, num)
-    return render_template("news.html", output = out, user=username)
-  else:
+    """
+    Render the news page. Display news articles based on the user's query if the request method is POST.
+
+    Returns:
+        str: Rendered HTML of the news page.
+    """
+    username = request.cookies.get("username")
+    if request.method == "POST":
+        query = request.form["news_query"]
+        num_articles = int(request.form["num_articles"])
+        news_data = model.get_search(query, num_articles)
+        return render_template("news.html", output=news_data, user=username)
+
     return render_template("news.html", user=username)
 
 
-#currencyConverter
 @app.route("/currencyConverter", methods=["POST", "GET"])
 def currency():
-  username = request.cookies.get("username")
-  if request.method == "POST":
-    num = int(request.form["num_currencies"])
-    out = sorted(datatypes.calculate_forex(datatypes.get_usd_rates(), num), key= lambda pair: pair['final_profit'], reverse = True)
-    return render_template("currencyConverter.html", output = out, lim=num, user=username)
-  else:
-    return render_template("currencyConverter.html", output = [], lim=0, user=username)
+    """
+    Render the currency converter page. Display currency conversion results if the request method is POST.
 
-#flashcards
+    Returns:
+        str: Rendered HTML of the currency converter page.
+    """
+    username = request.cookies.get("username")
+    if request.method == "POST":
+        num_currencies = int(request.form["num_currencies"])
+        forex_data = datatypes.calculate_forex(datatypes.get_usd_rates(), num_currencies)
+        sorted_data = sorted(forex_data, key=lambda pair: pair['final_profit'], reverse=True)
+        return render_template("currencyConverter.html", output=sorted_data, lim=num_currencies, user=username)
+
+    return render_template("currencyConverter.html", output=[], lim=0, user=username)
+
+
+def get_user_flashcards(username):
+    collection = db.profiles
+    user_data = collection.find_one({"username": username})
+    return user_data["flashcards"] if user_data else []
+
+
 @app.route("/flashcards", methods=["POST", "GET"])
 def flashcards():
-  collection = db.profiles
-  user_flashcards = collection.find_one({"username": request.cookies.get("username")})["flashcards"]
-  username = request.cookies.get("username")
+    """
+    Render the flashcards page. Generate an answer for a question if the request method is POST.
 
-  if request.method == "POST":
-    question = request.form["question"].lower().title()
-    answer = model.ask_ai(question, "flashcard")
-    return render_template("flashcards.html", input = question, output=answer, flashcards = user_flashcards, user=username)
-  else:
-    return render_template("flashcards.html", flashcards = user_flashcards, user=username)
+    Returns:
+        str: Rendered HTML of the flashcards page.
+    """
+    username = request.cookies.get("username")
+    user_flashcards = get_user_flashcards(username)
+
+    if request.method == "POST":
+        question = request.form["question"].lower().title()
+        answer = model.ask_ai(question, "flashcard")
+        return render_template("flashcards.html", input=question, output=answer,
+                               flashcards=user_flashcards, user=username)
+
+    return render_template("flashcards.html", flashcards=user_flashcards, user=username)
+
 
 @app.route("/save", methods=["POST", "GET"])
 def save_flashcards():
-  collection = db.profiles
-  query = {"username": request.cookies.get("username")}
-  user_flashcards = collection.find_one(query)["flashcards"]
-  username = request.cookies.get("username")
+    """
+    Render the flashcards page. Save a new flashcard if the request method is POST.
 
-  if request.method == "POST":
-    question = request.form["new_term"].lower().title()
-    answer = request.form["new_ans"]
-    #print(answer)
+    Returns:
+        str: Rendered HTML of the flashcards page.
+    """
+    username = request.cookies.get("username")
+    user_flashcards = get_user_flashcards(username)
 
-    new_flashcard = {
-      "topic" : question,
-      "conversation" : [ {
-        "author": "user",
-        "content" : answer
-      }]
-    }
-    user_flashcards = [new_flashcard] + user_flashcards
-    collection.update_one({"username": request.cookies.get("username")},{"$set": {"flashcards": user_flashcards} })
+    if request.method == "POST":
+        question = request.form["new_term"].lower().title()
+        answer = request.form["new_ans"]
 
-    return render_template("flashcards.html", flashcards = user_flashcards, user=username)
-  else:
-    return render_template("flashcards.html", flashcards = user_flashcards, user=username)
-  
-# @app.route("/user-flashcards")
-# def get_flashcards():
-#   username = request.cookies.get("username")
-#   collection = db.profiles
-#   flashcards = collection.find_one({"username":username})["flashcards"]
-#   print(flashcards)
-#   return flashcards
+        new_flashcard = {
+            "topic": question,
+            "conversation": [{"author": "user", "content": answer}]
+        }
+        user_flashcards = [new_flashcard] + user_flashcards
+        collection = db.profiles
+        collection.update_one({"username": username}, {"$set": {"flashcards": user_flashcards}})
+
+    return render_template("flashcards.html", flashcards=user_flashcards, user=username)
 
 
-
-#if successful login/signup, store in cookies and redirect to homepage
-
-# HANDLE POST AND GET for login
 @app.route("/")
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
-  if (request.method == "POST"):
-    collection = db.profiles
-    usr = request.form['username']
-    pwd = request.form['password']
-    verification = datatypes.input_verification(usr, pwd) 
-    if verification != "OK":
-      return render_template("login.html", error = verification)
-    
-    if request.form['action'] == "Signup":
-      new_user = datatypes.gen_new_user(usr, datatypes.hash(pwd))
-      if collection.find_one({"username":usr}) != None:
-        return render_template("login.html", error = "Username already exists")
-      collection.insert_one(new_user)
-    elif request.form['action'] == 'Login':
-      profile = collection.find_one({"username":usr})
-      if profile == None:
-        return render_template("login.html", error = "Username not found")
-      if not datatypes.password_check(pwd, profile['password']):
-        return render_template("login.html", error = "Incorrect password")
-    #verify
-    response = redirect("/home") #make_response()
-    response.set_cookie("username",request.form["username"])
-    response.set_cookie("password",request.form["password"])
-    
-    return response
-  else:
+    """
+    Handle user signup and login.
+
+    Returns:
+        str: Rendered HTML of the login page or a redirect to the home page.
+    """
+    if request.method == "POST":
+        collection = db.profiles
+        username = request.form['username']
+        password = request.form['password']
+        verification = datatypes.input_verification(username, password)
+        if verification != "OK":
+            return render_template("login.html", error=verification)
+
+        if request.form['action'] == "Signup":
+            return handle_signup(collection, username, password)
+        elif request.form['action'] == 'Login':
+            return handle_login(collection, username, password)
+
     response = make_response(render_template("login.html"))
     response.set_cookie("username", max_age=0)
     response.set_cookie("password", max_age=0)
-    return response #render_template("login.html")
+    return response
 
-def shutdown_server():
+
+def handle_signup(collection, username, password):
+    if collection.find_one({"username": username}):
+        return render_template("login.html", error="Username already exists")
+    new_user = datatypes.gen_new_user(username, datatypes.hash(password))
+    collection.insert_one(new_user)
+    return redirect_to_home(username, password)
+
+
+def handle_login(collection, username, password):
+    profile = collection.find_one({"username": username})
+    if not profile:
+        return render_template("login.html", error="Username not found")
+    if not datatypes.password_check(password, profile['password']):
+        return render_template("login.html", error="Incorrect password")
+    return redirect_to_home(username, password)
+
+
+def redirect_to_home(username, password):
+    response = redirect("/home")
+    response.set_cookie("username", username)
+    response.set_cookie("password", password)
+    return response
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    """
+    Shutdown the server.
+
+    Returns:
+        str: A message indicating that the server is shutting down.
+    """
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
-
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-    shutdown_server()
     return 'Server shutting down...'
 
-# # CLEAR OUT THE EVENTS
-# @app.route("/clear", methods=["POST"])
-# def clear_events():
-#   collection = db.events
-#   collection.delete_many({})
-#   return render_template("index.html")
-
-
-# db.profiles.delete_many({})
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
-
-
+    app.run(host='0.0.0.0', port=5000)
