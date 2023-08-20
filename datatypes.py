8970014675e4c68abbd7492bced42101a21371e8
Juan Landi <58494540+jdlandi@users.noreply.github.com>
2023-08-20 18:29:18 -0500

    Added Missing Method

--- datatypes.py
+++ datatypes.py
@@ -109,6 +109,58 @@ def input_verification(usr: str, pwd: str) -> str:
     return "OK"
 
 
+def gen_new_user(usr: str, pwd_hash: str) -> User:
+    return {
+        "username": usr,
+        "password": pwd_hash,
+        "name": "",
+        "email": "",
+        "private": True,
+        "location": "",
+        "following": [],
+        "flashcards": [{"topic": "What Are Different Ways Of Trading", "conversation": [
+            {
+                "author": "assistant",
+                "content": "Some common ways of trading include day trading, swing trading, positional trading, and algorithmic trading."
+            }
+        ]}, {"topic": "Why Do People Short", "conversation": [
+            {
+                "author": "assistant",
+                "content": "To profit from a decline in the price of an asset, or to hedge against potential losses in a portfolio."
+            }
+        ]}, {"topic": "What Is Citadel", "conversation": [
+            {
+                "author": "assistant",
+                "content": "Citadel is a global investment firm that manages hedge funds, operates market-making initiatives, and provides other financial services."
+            }
+        ]}, {"topic": "Can You Explain Equity", "conversation": [
+            {
+                "author": "assistant",
+                "content": "Equity represents ownership in a company, typically in the form of stocks. Higher equity means higher ownership and potential profits."
+            }
+        ]}, {"topic": "What Is Forex", "conversation": [
+            {
+                "author": "assistant",
+                "content": "Forex refers to the market for trading currencies from around the world. It is short for 'foreign exchange'"
+            }
+        ]}, {"topic": "What Are Bollinger Bands", "conversation": [
+            {
+                "author": "assistant",
+                "content": "Bollinger Bands are a technical analysis tool used to measure volatility. They consist of a moving average and upper and lower bands."
+            }
+        ]}
+                       ],
+        "messages": [],
+        "forexes": [],
+        "portfolios": [{"name": "AAPL", "creation": "04-01-2023",
+                        "private": True,
+                        "info": "<ul>\n<li>Investment firm Morgan Stanley raised Apple's price target to $190 based on the expectation of launching a virtual reality and augmented reality headset, which may dramatically expand the market.</li>\n<li>Yahoo Finance provides the latest Apple Inc. (AAPL) stock forecast based on top analysts' estimates, along with other investing and trading data.</li>\n<li>Apple is predicted for a small pullback, featuring a broken-wing butterfly trade that creates a profit zone between $155 and $175.</li>\n<li>Fred Alger Management released its Alger Spectra Fund's Q1 2023 investor letter, in which Apple was discussed for having outperformed in Q1.</li>\n<li>Despite any weakness in the market, holding onto AAPL stock is suggested, and buying at current prices is recommended as well.</li>\n</ul>\n<p>With Morgan Stanley raising its price target on Apple, it indicates that the prediction of an AR/VR headset launching appears to be the basis for this optimism. This product could substantially expand the market for Apple, thereby boosting its financial standing. This could lead to a rise in the company's stock price, causing a potentially profitable opportunity. However, the prediction of a small pullback in Apple's stock price may not affect its performance, given its positive forecast for the future. It is recommended for investors to buy AAPL stock at current prices or even hold onto it despite any existing weakness in the market, as the company has shown solid performance and is likely to outpace other contemporaries in the long run.</p>",
+                        "stocks": [{"ticker": "AAPL",
+                                    "orig_price": 100, "current_price": 23001,
+                                    "price_difference": 100, "day_high": 123212, "day_low": 1234}]}]
+    }
+
+
 def gen_stock(name: str) -> Stock:
     return {
         "ticker": name,
